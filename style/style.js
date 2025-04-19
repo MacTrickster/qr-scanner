500baac0a44d109fe6be0cecfba4eecb14700277
MacTrickster <nazar.hnid@gmail.com>
2025-04-19 22:40:54 +0300

    Create style.js

---
+++ style/style.js
@@ -0,0 +1,13 @@
+import { css } from 'styled-components';
+
+export const appStyles = css`
+  .container {
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 20px;
+    text-align: center;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  
+  // ... решта стилів з вашого оригінального коду ...
+`;
